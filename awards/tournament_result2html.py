b77811f6bc899d7e7935edc07afda26b67599f38
KokiHayashi <1234.884.koki@gmail.com>
2022-03-12 20:01:15 +0900

    kanto

--- awards/tournament_result2html.py
+++ awards/tournament_result2html.py
@@ -17,8 +17,8 @@ import sys
 print(sys.path)
 
 
-with open('output2022.txt', 'w') as fw:
-	with open('input2022.tsv', encoding='utf-8', newline='') as fr:
+with open('output_international_2020_2022.txt', 'w') as fw:
+	with open('input_international_2020_2022.tsv', encoding='utf-8', newline='') as fr:
 		counter = 0
 		memory = ''
 		for cols in csv.reader(fr, delimiter='\t'):
