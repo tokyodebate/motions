dbc0a46403f55cc0af3588c0501a68eaa22c4fe5
KokiHayashi <1234.884.koki@gmail.com>
2022-04-15 18:54:49 +0900

    その他の情報を加えました 2022/4/15

---
+++ その他の情報/gmail template/README.md
@@ -0,0 +1,4 @@
+utdsのgmailで大会の運営者にmotionを聴く際に送る文章のtemplateです
+
+python ./main.py
+
---
+++ その他の情報/gmail template/mail.py
@@ -0,0 +1,20 @@
+tournament_name = "AsianBP"
+year = "2021, 2020"
+yourname = "Koki Hayashi"
+
+
+text_message = f"""Dear the organizers of {tournament_name}. Thanks
+Hello, we are UTDS, University of Tokyo Debate Society from Japan.
+We are now collecting the motions of various English debate competitions all over the world.
+We are collecting the motions on our official UTDS website, so that everyone can see the motion and practice english debate.
+We would like to know the motions of {tournament_name} held in {year}, but we couldn't find it by using google search and facebook search.
+Would you mind telling us the URL of motion tabs of the {tournament_name} in {year}? If URL is not available, would you mind
+sending us how can we get the motion of {tournament_name}?
+It would be very helpful for us. Thank you.
+
+Best wishes,
+
+University of Tokyo Debate Society, Student member {yourname}
+"""
+
+print(text_message)
